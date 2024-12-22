0bc95568339b7f54c46d1525b8b186d8f06d1256
Asheet Bhaskar <asheet.iitj@gmail.com>
2024-12-23 02:37:47 +0530

    extracted mocks of respositories out of service tests

---
+++ db/repository/mock.go
@@ -0,0 +1,55 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/asheet-bhaskar/billing-service/app/models"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockBillRepository struct {
+	mock.Mock
+}
+
+type MockCurrencyRepository struct {
+	mock.Mock
+}
+
+type MockCustomerRepository struct {
+	mock.Mock
+}
+
+func (m *MockBillRepository) Create(ctx context.Context, bill *models.Bill) (*models.Bill, error) {
+	args := m.Called(ctx, bill)
+	return args.Get(0).(*models.Bill), args.Error(1)
+}
+
+func (m *MockBillRepository) GetByID(ctx context.Context, id int64) (*models.Bill, error) {
+	args := m.Called(ctx, id)
+	return args.Get(0).(*models.Bill), args.Error(1)
+}
+
+func (m *MockCurrencyRepository) Create(ctx context.Context, currency *models.Currency) (*models.Currency, error) {
+	args := m.Called(ctx, currency)
+	return args.Get(0).(*models.Currency), args.Error(1)
+}
+
+func (m *MockCurrencyRepository) GetByID(ctx context.Context, id int64) (*models.Currency, error) {
+	args := m.Called(ctx, id)
+	return args.Get(0).(*models.Currency), args.Error(1)
+}
+
+func (m *MockCurrencyRepository) GetByCode(ctx context.Context, code string) (*models.Currency, error) {
+	args := m.Called(ctx, code)
+	return args.Get(0).(*models.Currency), args.Error(1)
+}
+
+func (m *MockCustomerRepository) Create(ctx context.Context, customer *models.Customer) (*models.Customer, error) {
+	args := m.Called(ctx, customer)
+	return args.Get(0).(*models.Customer), args.Error(1)
+}
+
+func (m *MockCustomerRepository) GetByID(ctx context.Context, id int64) (*models.Customer, error) {
+	args := m.Called(ctx, id)
+	return args.Get(0).(*models.Customer), args.Error(1)
+}
