84808b551bc1a0cc710d33b1720b59fbd98fe2ac
Asheet Bhaskar <asheet.iitj@gmail.com>
2024-12-23 19:55:21 +0530

    added temporal in service dependecny

--- app/handlers/service.go
+++ app/handlers/service.go
@@ -1,9 +1,13 @@
 package handlers
 
 import (
+	"log"
+
 	service "github.com/asheet-bhaskar/billing-service/app/services"
 	"github.com/asheet-bhaskar/billing-service/db"
 	"github.com/asheet-bhaskar/billing-service/db/repository"
+	"github.com/asheet-bhaskar/billing-service/worker"
+	"go.temporal.io/sdk/client"
 )
 
 // encore:service
@@ -18,9 +22,16 @@ func initBillingService() (*BillingService, error) {
 	BillRepo := repository.NewBillRepository(dbClient.DB)
 	CustomerRepo := repository.NewCustomerRepository(dbClient.DB)
 	CurrencyRepo := repository.NewCurrencyRepository(dbClient.DB)
+	temporalClient, err := client.NewClient(client.Options{})
+	if err != nil {
+		log.Fatal("Failed to start temporal")
+	}
+
+	log.Println("starting temporal worker")
+	go worker.Start(temporalClient)
 
 	return &BillingService{
-		Bill:     service.NewBillService(BillRepo, CurrencyRepo, CustomerRepo),
+		Bill:     service.NewBillService(BillRepo, CurrencyRepo, CustomerRepo, temporalClient),
 		Customer: service.NewCustomerService(CustomerRepo),
 		Currency: service.NewCurrencyService(CurrencyRepo),
 	}, nil
